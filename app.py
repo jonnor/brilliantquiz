7a6a7d0f89c7470791afe2c3df4e697d76e7e9dc
Jon Nordby <jononor@gmail.com>
2019-12-08 23:52:04 +0100

    Add basic quiz progression logic

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 import quiz2020
 
+print('N questions', len(quiz2020.questions), quiz2020.questions[-1])
 
-@app.route('/')
-def index():
-    question_no = 0
+
+@app.route('/question/<int:question_no>')
+def get_question(question_no):
+
+    if question_no == len(quiz2020.questions):
+        return flask.redirect(f'/done')
+
 
     info = quiz2020.questions[question_no]
 
@@ -17,12 +22,26 @@ def index():
 
     html = flask.render_template('quiz.html',
 
-            question_no=question_no+1,
+            question_no=question_no,
             question=question,
             options=options
     )
 
     return html
+
+@app.route('/answer/<int:question_no>')
+def give_answer(question_no):
+    next = question_no + 1
+    return flask.redirect(f'/question/{next}')
+
+@app.route('/done')
+def get_done():
+    return "Congrats! Have a nice new year"
+
+
+@app.route('/')
+def index():
+    return flask.redirect('/question/0')
 
 
 if __name__ == '__main__':
