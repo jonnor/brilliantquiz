bbbf3329a5720fe6a7c52985345f0adf1948faca
Jon Nordby <jononor@gmail.com>
2019-12-19 22:40:06 +0100

    Show correct answer after each step

--- TODO.md
+++ TODO.md
@@ -3,10 +3,11 @@
 ##
 
 - Keep track of the score
-- Show correct option after each answer
 - Complete the concrats screen
 
 - Test on mobile phone and desktop
 
 - Add footer with Quiz by Marianna Brilliantova. Link to website? Instagram?
 - Add Share on Facebook button
+
+
--- app.py
+++ app.py
@@ -12,6 +12,41 @@ print('N questions', len(quiz2020.questions))
 print('images', len(images), images)
 
 
+def render_question(question_no,
+                    submit_text='', submit_url='',
+                    checked_idx=None, show_answer=False):
+    info = quiz2020.questions[question_no]
+
+    question = info[0]
+    options = info[1]
+    correct = info[2]
+
+    image_url = flask.url_for('static', filename=os.path.join('images', images[question_no]))
+
+    # Keep the given answer checked
+    checks = [ checked_idx == i for i, _ in enumerate(options) ]
+
+    # Mark the correct answer
+    if show_answer:
+        corrects = [ o == correct for o in options ]
+    else:
+        corrects = [ False ] * len(options)
+
+    print(options, checks)
+
+    html = flask.render_template('quiz.html',
+            submit_url=submit_url,
+            submit_text=submit_text,
+            image_url=image_url,
+            question_no=question_no,
+            question=question,
+            options=options,
+            options_checked=checks,
+            options_correct=corrects,
+    )
+    return html
+
+
 @app.route('/question/<int:question_no>')
 def get_question(question_no):
 
@@ -19,28 +54,39 @@ def get_question(question_no):
         return flask.redirect(f'/done')
 
 
-    info = quiz2020.questions[question_no]
-
-    question = info[0]
-    options = info[1]
-    correct = info[2]
-
-
-    image_url = flask.url_for('static', filename=os.path.join('images', images[question_no]))
-
-    html = flask.render_template('quiz.html',
-            image_url=image_url,
-            question_no=question_no,
-            question=question,
-            options=options
-    )
+    html = render_question(question_no,
+            submit_text="Submit answer",
+            submit_url=f"/answer/{question_no}")
 
     return html
 
 @app.route('/answer/<int:question_no>')
 def give_answer(question_no):
+
+    info = quiz2020.questions[question_no]
+    options = info[1]
+    correct = info[2]
+
+    answer = flask.request.args.get('answer', '')
+
+
+    # Keep selected option checked
+    try:
+        answer_idx = options.index(answer)
+    except ValueError:
+        answer_idx = None
+    # TODO: check if correct answer, increase score
+    #if answer == correct:
+    #    score += 1
+
     next = question_no + 1
-    return flask.redirect(f'/question/{next}')
+    html = render_question(question_no,
+                    submit_text="Next",
+                    submit_url=f'/question/{next}',
+                    checked_idx=answer_idx,
+                    show_answer=True)
+
+    return html
 
 @app.route('/done')
 def get_done():
